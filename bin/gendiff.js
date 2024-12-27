e62d1d580df36a638c96a2abe34bdbcda4449f77
Stitchh626 <arttrumen@mail.ru>
2024-12-27 06:33:03 +0200

    Add Step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { version } = require('../package.json'); // Импорт версии из package.json
 
 const program = new Command();
 
-const command = () => {
-  console.log('Hello, World!');
-};
-
 program
-  .version('0.0.1')
+  .version(version, '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .action(command)
+  .argument('<filepath1>', 'path to the first file')
+  .argument('<filepath2>', 'path to the second file')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
   .parse(process.argv);
+
+const options = program.opts();
+const [filepath1, filepath2] = program.args;
+
+console.log('Comparing files:');
+console.log(`File 1: ${filepath1}`);
+console.log(`File 2: ${filepath2}`);
+console.log(`Format: ${options.format || 'default'}`);
